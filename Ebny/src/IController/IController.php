9a510ec16ef2f2d83d3aebe3477cb66697b0685b
Ebba Nyberg <nyberg.ebba@hotmail.com>
2013-06-09 21:15:08 +0200

    deleted

--- Ebny/src/IController/IController.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Interface for classes implementing a controller.
- *
- * @package EbnyCore
- */
-interface IController {
-  public function Index();
-}
--- Ebny/src/ISingleton/ISingleton.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Interface for classes implementing the singleton pattern.
- *
- * @package EbnyCore
- */
-interface ISingleton {
-  public static function Instance();
-}
